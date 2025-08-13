4be0e1c88264694e5dedfcca6ac9cb7e76c5ad96
Euan R <rippereuan@gmail.com>
2025-08-13 12:18:16 -0400

    Update sketch.js
    current state: plays out the game with randomly selected cells, trying to allow cells to be brought to life by mouse but thats not quite working

--- sketch.js
+++ sketch.js
@@ -1,31 +1,75 @@
 let grid = [];
 let cols = 50;
 let rows = 50;
-let cellW, cellH;
+let cellWidth, cellHeight;
 
 function setup() {
   createCanvas(620, 620);
-  cellW = width / cols;
-  cellH = height / rows;
+  cellWidth = width / cols;
+  cellHeight = height / rows;
 
   for (let i = 0; i < cols; i++) {
-    grid.push([]);
+    grid[i] = [];
     for (let j = 0; j < rows; j++) {
-      grid[i].push(Math.floor(Math.random() * 2)); 
+      grid[i][j] = floor(random(2));
     }
   }
 }
 
+function mouseClicked(){
+  let col = floor(mouseX / cellWidth);
+  let row = floor(mouseY / cellHeight);//work out which cell im mousing over
+  grid[row][col] = 1//set that cell to alive
+  fill(0);//colour it in
+  rect(row * cellHeight, col * cellWidth, cellWidth, cellHeight);
+  //rect(row * cellWidth, coll * cellHeight, cellWidth, cellHeight);//ts not working
+}
+
 function draw() {
   background(255);
+  let nextGrid = [];
+
   for (let i = 0; i < cols; i++) {
+    nextGrid[i] = [];
     for (let j = 0; j < rows; j++) {
-      if (grid[i][j] === 1) {
-        fill(0); 
-      } else {
-        fill(255); 
+      let neighbours = 0;
+
+      //check each adjacent cell
+      for (let xOff = -1; xOff <= 1; xOff++) {
+        for (let yOff = -1; yOff <= 1; yOff++) {
+          if (xOff === 0 && yOff === 0) continue; //not including the current cell
+          let x = i + xOff;
+          let y = j + yOff;
+          if (x >= 0 && x < cols && y >= 0 && y < rows) {
+            neighbours += grid[x][y];
+          }
+        }
       }
-      rect(i * cellW, j * cellH, cellW, cellH);
+
+      //CGOL logic
+      if (grid[i][j] === 1) {
+        if (neighbours < 2 || neighbours > 3) {
+          nextGrid[i][j] = 0; //dies lol
+        } else {
+          nextGrid[i][j] = 1; //stays alive
+        }
+      } else {
+        if (neighbours === 3) {
+          nextGrid[i][j] = 1; //brought to life
+        } else {
+          nextGrid[i][j] = 0;
+        }
+      }
+
+      //draw
+      if (grid[i][j] === 1) {
+        fill(0);
+      } else {
+        fill(255);
+      }
+      rect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
     }
   }
+
+  grid = nextGrid;
 }
